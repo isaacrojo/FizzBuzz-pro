989a2c03f02ca7fd45ca34fb869e88ad536ca1d0
isaacrojo <60981406+isaacrojo@users.noreply.github.com>
2020-03-04 23:16:36 -0700

    FizzBuzz super mega optimizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P_7_FizzBuzz_pro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+           bool isFizzOrBuzz = false; 
+           string FizzBuzzMessage = 
+           
+            for(int i = 0; i<= 100; i++)
+            {
+                 if(i % 3 == 0)
+                {
+                    Console.Write("Fizz");
+                }                                 //un truco es usar Console.Write, sin 'Line' para juntar FizzBuzz en una sola linea a no no cierto
+                 if (i % 5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+                
+            }
+        }
+        
+    }
+}
